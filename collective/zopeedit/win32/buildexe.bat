871d07aa69b187ddda686d15e227b53a0698c7c6
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-02-07 17:19:45 +0100

    Use python 2.7

--- collective/zopeedit/win32/buildexe.bat
+++ collective/zopeedit/win32/buildexe.bat
@@ -1,4 +1,4 @@
 cd ..\..\..
-c:\python26\python setup.py py2exe -p Plugins -p win32com -e Tkinter -p encodings
+c:\python27\python setup.py py2exe -p Plugins -p win32com -e Tkinter -p encodings
 cd collective\zopeedit\win32
 pause
